f53376b720b282c0df3cb9b50fa4a6ee74ff9572
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-22 10:00:36 -0600

    register form added/verify passwords

--- server.js
+++ server.js
@@ -11,18 +11,36 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
+// home page
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// login form is already registered
 app.get('/login', (req, res) => {
   res.render('login');
 });
-
-
 app.post('/login', (req, res) => {
   res.redirect('/');
 });
+
+
+/// registration form
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+/// password verification on registration form
+app.post('/register', (req, res) => {
+  if (req.body.password === req.body.verify) {
+    res.redirect('/login');
+  } else {
+    res.render('register', {
+      email: req.body.email,
+      message: 'Passwords do not match'
+    });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
