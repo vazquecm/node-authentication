6dd455f6658de686a0f65cf90aae690c9b399b33
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-22 09:46:46 -0600

    installed body-parser, login form

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-authentication
+
+##In class exercise
+
+##Technology Used
+
+1. Express
+2. Jade
+3. body-parser
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 'use strict';
 
+
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
 
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+app.post('/login', (req, res) => {
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
