0a4dadac216e1c8ec3bda7847aba26f10c4b6dcd
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-23 10:27:48 -0600

    installed method-override/logout

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);// for memory storage
@@ -11,7 +12,6 @@ const userRoutes = require('./lib/user/routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 // use the first part if it exists if not use the second part
 const SESSION_SECRET = process.env.SESSION_SECRET || 'supersecret';
 
@@ -19,6 +19,7 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
+app.use(methodOverride('_method'));
 // keeps the info stored in a session ID cookie for user tracking
 app.use(session({
   secret: SESSION_SECRET,
