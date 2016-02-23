73b64fd1dddde61fa99ca8d7f524737679fc0cd7
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-23 11:50:05 -0600

    passport working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);// for memory storage
 
@@ -27,12 +28,15 @@ app.use(session({
   store: new RedisStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(userRoutes);
 
 app.locals.title = '';
 
 app.use((req, res, next) => {
-  app.locals.user = req.session.user || { email: 'Guest' };
+  res.locals.user = req.user;
   next();
 });
 
